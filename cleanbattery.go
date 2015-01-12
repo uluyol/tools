34bbaeb6ca000538960365a36d2da8fad1116de4
Muhammed Uluyol <uluyo002@umn.edu>
2015-01-12 07:55:57 -0600

    Update style, follow Go conventions

--- cleanbattery.go
+++ cleanbattery.go
@@ -1,29 +1,31 @@
 /*
- *  Copyright (c) 2012, Muhammed Uluyol <uluyol0@gmail.com>
- *  All rights reserved.
- *
- *  Redistribution and use in source and binary forms, with or without
- *  modification, are permitted provided that the following conditions are met:
- *
- *   - Redistributions of source code must retain the above copyright notice,
- *     this list of conditions and the following disclaimer.
- *
- *   - Redistributions in binary form must reproduce the above copyright notice,
- *     this list of conditions and the following disclaimer in the documentation
- *     and/or other materials provided with the distribution.
- *
- *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
- *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
- *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
- *  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- *  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
- *  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
- *  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
- *  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
- *  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
- *  POSSIBILITY OF SUCH DAMAGE.
- */
+
+  Copyright (c) 2012-2014, Muhammed Uluyol <uluyol0@gmail.com>
+  All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without
+  modification, are permitted provided that the following conditions are met:
+
+   - Redistributions of source code must retain the above copyright notice,
+     this list of conditions and the following disclaimer.
+
+   - Redistributions in binary form must reproduce the above copyright notice,
+     this list of conditions and the following disclaimer in the documentation
+     and/or other materials provided with the distribution.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+  LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+  CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+  INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+  CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+  POSSIBILITY OF SUCH DAMAGE.
+
+*/
 
 package main
 
@@ -38,14 +40,14 @@ import (
 )
 
 const (
-	TIMEOUT            = 3000
-	NEWLINE            = byte(10)
-	STATUS_CHARGING    = "Charging"
-	STATUS_DISCHARGING = "Discharging"
+	timeout            = 3000
+	newline            = byte(10)
+	statusCharging    = "Charging"
+	statusDischarging = "Discharging"
 )
 
 var (
-	status_icon *gtk.GtkStatusIcon
+	statusIcon *gtk.GtkStatusIcon
 	full        int64
 )
 
@@ -57,22 +59,22 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	str := string(bytes.Split(buf, []byte{NEWLINE})[0])
+	str := string(bytes.Split(buf, []byte{newline})[0])
 	full, err = strconv.ParseInt(str, 10, 64)
 	if err != nil {
 		panic(err)
 	}
 
-	status_icon = gtk.StatusIcon()
-	status_icon.SetTitle("zzcleanbattery")
+	statusIcon = gtk.StatusIcon()
+	statusIcon.SetTitle("zzcleanbattery")
 
-	update_icon()
+	updateIcon()
 
-	glib.TimeoutAdd(TIMEOUT, update_icon)
+	glib.TimeoutAdd(timeout, updateIcon)
 	gtk.Main()
 }
 
-func update_icon() bool {
+func updateIcon() bool {
 
 	var (
 		hours   int64
@@ -89,7 +91,7 @@ func update_icon() bool {
 	if err != nil {
 		panic(err)
 	}
-	str := string(bytes.Split(buf, []byte{NEWLINE})[0])
+	str := string(bytes.Split(buf, []byte{newline})[0])
 	now, err = strconv.ParseInt(str, 10, 64)
 	if err != nil {
 		panic(err)
@@ -99,7 +101,7 @@ func update_icon() bool {
 	if err != nil {
 		panic(err)
 	}
-	str = string(bytes.Split(buf, []byte{NEWLINE})[0])
+	str = string(bytes.Split(buf, []byte{newline})[0])
 	rate, err = strconv.ParseInt(str, 10, 64)
 	if err != nil {
 		panic(err)
@@ -109,15 +111,15 @@ func update_icon() bool {
 	if err != nil {
 		panic(err)
 	}
-	status = string(bytes.Split(buf, []byte{NEWLINE})[0])
+	status = string(bytes.Split(buf, []byte{newline})[0])
 
 	pfull = now * 100 / full
 
 	if rate > 0 {
 		switch status {
-		case STATUS_CHARGING:
+		case statusCharging:
 			seconds = 3600 * (full - now) / rate
-		case STATUS_DISCHARGING:
+		case statusDischarging:
 			seconds = 3600 * now / rate
 		default:
 			seconds = 0
@@ -139,13 +141,13 @@ func update_icon() bool {
 			minutes)
 	}
 
-	status_icon.SetTooltipText(text)
-	status_icon.SetFromIconName(get_icon_name(status, pfull))
+	statusIcon.SetTooltipText(text)
+	statusIcon.SetFromIconName(getIconName(status, pfull))
 	return true
 }
 
-func get_icon_name(status string, pfull int64) string {
-	if status == STATUS_DISCHARGING {
+func getIconName(status string, pfull int64) string {
+	if status == statusDischarging {
 		switch {
 		case pfull < 10:
 			return "battery_empty"
@@ -160,7 +162,7 @@ func get_icon_name(status string, pfull int64) string {
 		default:
 			return "battery_full"
 		}
-	} else if status == STATUS_CHARGING {
+	} else if status == statusCharging {
 		return "battery_charged"
 	}
 	return "battery_plugged"
