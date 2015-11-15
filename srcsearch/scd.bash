ec53ad1db48e118c0e0d368d42043e18850baa5e
Muhammed Uluyol <uluyol0@gmail.com>
2015-11-14 23:39:33 -0500

    Add function for using srcsearch relative to working directory

--- srcsearch/scd.bash
+++ srcsearch/scd.bash
@@ -7,3 +7,13 @@ scd() {
 	printf "%s\n" "$d"
 	cd "$d"
 }
+
+ccd() {
+	d=$(env SRCSEARCHROOT="$PWD" srcsearch "$@")
+	st=$?
+	if [[ $st -ne 0 ]]; then
+		return $st
+	fi
+	printf "%s\n" "$d"
+	cd "$d"
+}
