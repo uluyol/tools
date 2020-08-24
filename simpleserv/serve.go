9ca1d6f11e0b81ee983cf9f5ce7714ffec96b00b
Muhammed Uluyol <uluyol0@gmail.com>
2020-08-24 19:49:13 -0400

    simpleserv: support basic auth

--- simpleserv/serve.go
+++ simpleserv/serve.go
@@ -1,25 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"strconv"
 )
 
-func main() {
-	var err error
-	port := int64(8081)
-	if len(os.Args) > 1 {
-		port, err = strconv.ParseInt(os.Args[1], 10, 64)
-		if err != nil {
-			log.Fatal(err)
+type ProtectedHandler struct {
+	H    http.Handler
+	User string
+	Pass string
+}
+
+func (h *ProtectedHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if h.User != "" && h.Pass != "" {
+		w.Header().Set("WWW-Authenticate", `Basic realm="Restricted"`)
+
+		user, pass, authOK := r.BasicAuth()
+		if authOK == false {
+			http.Error(w, "Not authorized", 401)
+			return
+		}
+
+		if user != h.User || pass != h.Pass {
+			http.Error(w, "Not authorized", 401)
+			return
 		}
 	}
-	fmt.Printf("Serving files in the current directory on port %d\n", port)
-	http.Handle("/", http.FileServer(http.Dir(".")))
-	err = http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
+
+	h.H.ServeHTTP(w, r)
+}
+
+func main() {
+	var (
+		port = flag.Int("port", 8081, "port to serve on")
+		user = flag.String("u", "", "user to protect with")
+		pass = flag.String("p", "", "password to protect with")
+	)
+	flag.Parse()
+
+	log.Printf("Serving files in the current directory on port %d", *port)
+	http.Handle("/", &ProtectedHandler{http.FileServer(http.Dir(".")), *user, *pass})
+	err := http.ListenAndServe(fmt.Sprintf(":%d", *port), nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
